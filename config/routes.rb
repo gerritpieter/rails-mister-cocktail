c0ab09851f34225e86ed8c120b6b3518edf460ab
gerrit pieter van t riet <geertriet@gmail.com>
2020-09-10 22:41:47 +0200

    day 1 of cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'doses/new'
+  root to: 'cocktails#index'
+  resources :cocktails, except: [:index] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
 end
